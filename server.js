24a80ad3bd4fad8449a9e809c1cc3b2c42dc4082
MarikaYamao <mark.y.89121569@gmail.com>
2021-08-18 16:39:16 +0900

    error対応

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.get("/api/hello", function (req, res) {
 app.get("/api/:date", function (req, res) {
   const isDate = /^\d{4}-?\d{1,2}-?\d{1,2}$/.test(req.params.date)
   const date = isDate ? new Date(req.params.date) : new Date(Number(req.params.date))
+  if(!date.getTime()) res.json({error: 'Invalid Date'})
   res.json({unix: date.getTime(), utc: date.toUTCString()});
 });
 
